8873aa18a784525e4be2ff52e65312502b42db48
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-01-27 20:19:03 +0200

    JS Function

--- SnapChat/script.js
+++ SnapChat/script.js
@@ -1,14 +1,14 @@
-function checkLoginColor(){
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  var login_btn = document.getElementById('loginP');
+function checkLoginColor() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	var login_btn = document.getElementById("loginP");
 
-  if (username !== '' && password !== ''){
-    login_btn.classList.add('yellow');
-      } else {
-        login_btn.classList.remove('yellow');
-      }
-  }
-  function login_btn(){
-    alert("Verifying credentials");
-  }
+	if (username !== "" && password !== "") {
+		login_btn.classList.add("yellow");
+	} else {
+		login_btn.classList.remove("yellow");
+	}
+}
+function login_btn() {
+	alert("Verifying credentials");
+}
